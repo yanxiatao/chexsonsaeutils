fc022a2654f74e1d691054c7d5cf44207d07cba3
yanxiatao <63758728+yanxiatao@users.noreply.github.com>
2026-06-21 18:26:33 +0800

    Update src/main/java/git/chexson/chexsonsaeutils/pattern/replacement/ProcessingSlotCandidateGroup.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/git/chexson/chexsonsaeutils/pattern/replacement/ProcessingSlotCandidateGroup.java
+++ src/main/java/git/chexson/chexsonsaeutils/pattern/replacement/ProcessingSlotCandidateGroup.java
@@ -8,4 +8,6 @@ public record ProcessingSlotCandidateGroup(
         ResourceLocation tagId,
         List<ResourceLocation> itemIds
 ) {
-}
+public ProcessingSlotCandidateGroup {
+        itemIds = List.copyOf(itemIds);
+    }
